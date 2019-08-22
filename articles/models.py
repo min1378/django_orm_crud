e1eb50097c10ad79d2181e7ff5e8007c9b7afc9e
윤성민 <qwes123@naver.com>
2019-08-22 12:09:18 +0900

    Create README.md

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+
+# 1. python manage.py makemigrations => django한테 model 작성했음을 알림
+# 2. python manage.py migrate => django한테 실제 DB에 작성함
 # Create your models here.
 
 
@@ -8,10 +11,13 @@ class Article(models.Model):
     #id = models.AutoField(primary_key=True)
     
     
+    # 모든 필드는 기본적으로 NOT NULL => 비어있으면 안된다.
     # CharField 에서는 max_length가 필수 인자다.
     title = models.CharField(max_length=20) # 클래스 변수 (DB의 필드)
     content = models.TextField() # 클래스 변수 (DB의 필드)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
-
+    def __str__(self):
+        return f'{self.id} - {self.title} : {self.content}'
 
